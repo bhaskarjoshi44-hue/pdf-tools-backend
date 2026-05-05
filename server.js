4d17e82d39bde80f100c75d3377bec8ff032a6a4
bhaskarjoshi44-hue <bhaskarjoshi44@gmail.com>
2026-05-05 23:09:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -115,3 +115,6 @@ setInterval(() => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.get('/', (req, res) => {
+    res.status(200).send("Smart PDF Backend is Live and Running!");
+});
